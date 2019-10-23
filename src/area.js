75c9906b3842cefcebafabdd795dd9a52c74bc8e
michaelthr33 <MStaunton@ucvts.tec.nj.us>
2019-10-23 10:28:27 -0400

    converted strings to numbers

--- src/area.js
+++ src/area.js
@@ -3,5 +3,7 @@ var readlineSync = require('readline-sync');
 const width = readlineSync.question('Width:');
 const length = readlineSync.question('Length:');
 const mm = 25.4;
-const area = width*length*mm
+let l=number(length);
+let w=number(width);
+const area = l*w*mm
 console.log("A(n)"+ width +'-by-'+length+'-inch sheet of paper has a area of '+area+' square millimeter(s)');
