ebadd2ca61ffd236eec64f6907abc5e03a16e3ea
michaelthr33 <MStaunton@ucvts.tec.nj.us>
2019-10-23 10:34:40 -0400

    repaired variables

--- src/cornhole.js
+++ src/cornhole.js
@@ -7,5 +7,5 @@ const r= 6/2
 const cornarea = l*w
 const holearea = Math.pow(r,2)
 const holearea2=holearea*3.1415
-const cornholearea= cornarea - holearea
+const cornholearea= cornarea - holearea2
 console.log("The surface area of a standard Cornhole board is " + cornholearea + " square inch(es)")
