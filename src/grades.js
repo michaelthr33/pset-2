68fa1fc6e02a55ee01d807da6958557093ae9896
michaelthr33 <MStaunton@ucvts.tec.nj.us>
2019-10-25 10:51:16 -0400

    grade

--- src/grades.js
+++ src/grades.js
@@ -2,8 +2,11 @@ console.log('enter three homework grades');
 var readlineSync = require('readline-sync');
 
 let hw1= readlineSync.question();
+let homework1=Number(hw1);
 let hw2= readlineSync.question();
+let homework2=Number(hw2);
 let hw3= readlineSync.question();
+let homework3=Number(hw3);
 console.log("enter three quiz grades")
 let q1= readlineSync.question();
 let q2= readlineSync.question();
