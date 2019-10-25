f28be908efec40f85388263a37bbd9d085a7dd11
michaelthr33 <MStaunton@ucvts.tec.nj.us>
2019-10-25 09:31:46 -0400

    re formatted area code, created grades.js

--- src/area.js
+++ src/area.js
@@ -5,5 +5,6 @@ const length = readlineSync.question('Length:');
 const mm = 25.4;
 let l=number(length);
 let w=number(width);
-const area = l*w*mm
+let area = l*w*mm
+let area = toLocaleString('en-us')
 console.log("A(n)"+ width +'-by-'+length+'-inch sheet of paper has a area of '+area+' square millimeter(s)');
