edfc660be1505be43f21800c668892be35c0f76a
michaelthr33 <MStaunton@ucvts.tec.nj.us>
2019-10-25 09:42:58 -0400

    grades.js ui created

--- src/grades.js
+++ src/grades.js
@@ -1,11 +1,16 @@
+console.log('enter three homework grades');
 var readlineSync = require('readline-sync');
 
-let w = readlineSync.question('Width:');
-let l = readlineSync.question('Length:');
-let a = Number(w);
-let b = Number(l);
-let ca = a*a;
-let cb = b*b
-let c = cb+ca;
-let diagonal = Math.pow(c,.5);
-console.log("A(n)"+ w +'-by-'+l+'-inch sheet of paper has a diagonal of '+diagonal+' inch(es)');
+let hw1= readlineSync.question();
+let hw2= readlineSync.question();
+let hw3= readlineSync.question();
+console.log("enter three quiz grades")
+let q1= readlineSync.question();
+let q2= readlineSync.question();
+let q3= readlineSync.question();
+console.log("enter three test grades");
+let t1= readlineSync.question();
+let t2= readlineSync.question();
+let t3 = readlineSync.question();
+
+console.log();
